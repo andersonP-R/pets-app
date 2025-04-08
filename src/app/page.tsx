b743035b2017693d7f08b866f60c0d1d54a389ec
Yesika rodriguez <alerodriguezc3@gmail.com>
2025-04-08 02:56:50 -0500

    Actaulizacion del home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import HomePage from "@/app/home/inicio/page";
+import Encabezado from "@/components/homeStatic/headerMenu";
+import PiePagina from "@/components/homeStatic/footer";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Inicio</h1>
+    <div >
+      <Encabezado />
+      <HomePage/>
+      <PiePagina />
     </div>
   );
 }
